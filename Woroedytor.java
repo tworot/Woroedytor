348e455718683c94d444b05b5e378a22249ccb84
tworo <tworot@gmail.com>
2019-05-04 21:32:55 +0200

    git commit

--- Woroedytor.java
+++ Woroedytor.java
@@ -1,7 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.lang.Math.*;
 import java.util.*;
 import java.io.*;
 
@@ -52,7 +51,6 @@ public class Woroedytor {
     private static int kolOff=0;  //(x) względem okna a położeniem w liście "wpisane"
     private static int kolOff2=0; //zmienna pomocnicza, jest o jeden większa od 
     private static ArrayList<String> wpisane=new ArrayList<String>(linMax);
-    static final Kursor kursor = new Kursor(0,0,kurSzer,kurWys,kurColor,kurColor);
     private static boolean menuWyjscia = false;
     private static boolean edytowany = false;
 
@@ -117,34 +115,73 @@ public static void main(String[] args) {
 		
 	String nazwaPliku = new String(nameFinder(args));
   	JLabel labels[] = new JLabel[linMax];
-    JFrame ramka = new JFrame("Woroedytor");
-	ramka.setContentPane(kursor);
-	ramka.getContentPane().setBackground(backgroundColor);
+	JFrame mainWindow = new JFrame("Woroedytor");
+	JLayeredPane lp = new JLayeredPane();
+    mainWindow.setBounds(200,0,kolMax*kurSzer+25,linMax*kurWys+55);
+	//ramka.setContentPane(tekst);
+	//ramka.getContentPane().setBackground(backgroundColor);
+	mainWindow.setResizable(false);
+    mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	mainWindow.add(lp);
+
+	JPanel textPane = new JPanel();
 	// ramka.getContentPane().setBackground(Color.RED);
-    ramka.setLayout(new BoxLayout(ramka.getContentPane(), BoxLayout.Y_AXIS));
-    ramka.setBounds(200,0,kolMax*kurSzer+25,linMax*kurWys+55);
-	ramka.setResizable(false);
+	textPane.setLayout(new BoxLayout(textPane, BoxLayout.Y_AXIS));
+	textPane.setBackground(backgroundColor);
+	textPane.setSize(kolMax*kurSzer+25,linMax*kurWys+55);
+	//ramka.add(kursor);
 	
-    ramka.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	for(int i=0;i<labels.length;i++){
 		labels[i] = new JLabel(" ");
 		labels[i].setFont(new Font("Courier New", Font.PLAIN, fontRozmiar));
     	labels[i].setForeground(textColor);
     	labels[i].setPreferredSize(new Dimension(kolMax*kurSzer,kurWys));
     	labels[i].setMinimumSize(new Dimension(1,kurWys));
-    	ramka.add(labels[i]);}
+		//labels[i].setLocation(0,kurWys*i);
+    	textPane.add(labels[i]);}
 	JLabel status = new JLabel("Ln 1, Kol 1");
 	status.setFont(new Font("Courier New", Font.PLAIN, 12));
 	status.setForeground(textColor);
+	textPane.add(status);
 
-	ramka.add(status);
-	
 	status.setText("Ln 1, Kol 1 - "+openFile(nazwaPliku,args));
 	refreshRows(labels);
-	ramka.setTitle(nazwaPliku + " - Woroedytor");
+	mainWindow.setTitle(nazwaPliku + " - Woroedytor");
 
-		
-	ramka.addKeyListener(
+
+	JLabel cursorLetter = new JLabel(" ");
+	cursorLetter.setFont(new Font("Courier New", Font.PLAIN, fontRozmiar));
+	cursorLetter.setForeground(Color.BLACK);
+	cursorLetter.setLocation(0,0);
+	cursorLetter.setSize(cursorLetter.getPreferredSize());
+
+	@SuppressWarnings("serial")   
+	JPanel cursor = new JPanel(){
+		@Override      
+		public void paintComponent(Graphics g){
+			super.paintComponent(g);
+			Graphics2D g2d = (Graphics2D) g;
+			cursorLetter.setLocation(kurSzer*kolumna,kurWys*linia);
+			if (kolumna+kolOff2>=wpisane.get(linia+linOff).length()) cursorLetter.setText(" ");
+			else cursorLetter.setText(wpisane.get(linia+linOff).substring(kolumna+kolOff2,kolumna+kolOff2+1));
+			g2d.setColor(kurColor);
+			g2d.fillRect(kurSzer*kolumna,kurWys*linia,kurSzer,kurWys);
+			g2d.setColor(textColor);
+			g2d.drawRect(kurSzer*kolumna,kurWys*linia,kurSzer,kurWys);
+		}};
+
+	cursor.setLayout(null);
+	cursor.setOpaque(true);
+	cursor.add(cursorLetter);
+	cursor.setSize(kolMax*kurSzer+25,linMax*kurWys+55);
+	cursor.setOpaque(false);
+	cursor.repaint();
+	
+	lp.add(textPane,Integer.valueOf(1));
+	lp.add(cursor,Integer.valueOf(2));
+	lp.setVisible(true);
+
+	mainWindow.addKeyListener(
       new KeyAdapter() {
         public void keyPressed(KeyEvent e) {
         	int keyCode = e.getKeyCode();
@@ -152,7 +189,7 @@ public static void main(String[] args) {
             if(czyDebug) System.out.println("Numer znaku: "+(int) c+"\nKod klawisza: "+keyCode);
 			if (c == 19) { //Ctrl + S
 				if(saveFile(nazwaPliku)){
-                    ramka.setTitle(nazwaPliku + " - Woroedytor");
+                    mainWindow.setTitle(nazwaPliku + " - Woroedytor");
                     edytowany = false;
 				    status.setText("Ln "+(linia+linOff+1)+", Kol "+(kolumna+kolOff2+1)+" - ZAPISANE");}}
             else if (c == 27) enteredEscape(status); //Escape
@@ -169,8 +206,7 @@ public static void main(String[] args) {
         if (keyCode == KeyEvent.VK_LEFT) wLewo(labels);        	
         if (keyCode == KeyEvent.VK_RIGHT) wPrawo(labels); }       	
         
-        else if (c == 10) enteredEnter(labels); //Enter / Ctrl+J
-            
+        else if (c == 10) enteredEnter(labels); //Enter / Ctrl+J            
         else if (c == 127) enteredDelete(labels); //Delete
 		else if (c == 8) enteredBackspace(labels); //Backspace / Ctrl+H
         else if (c != 0xffff && c>31) { 
@@ -184,24 +220,24 @@ public static void main(String[] args) {
         if(!(wpisane.get(wpisane.size()-1).equals(""))) wpisane.add(""); //tworzy ostatnią pustą linię
 				
 		naprawKolumny();	
-        if(edytowany) ramka.setTitle("* "+nazwaPliku + " - Woroedytor");
+        if(edytowany) mainWindow.setTitle("* "+nazwaPliku + " - Woroedytor");
         labels[linia].setText(napiszAkt(wpisane.get(linia+linOff)));
-        kursor.setKursor(linia,kolumna);
-		kursor.revalidate();
-		ramka.revalidate();
-		ramka.repaint();
+        //cursor.setKursor(linia,kolumna);
+		cursor.revalidate();
+		mainWindow.revalidate();
+		mainWindow.repaint();
 		if(czyDebug) System.out.println("Kolumna: "+kolumna+" Linia: "+linia);}
       }
     );
     
-		ramka.setVisible(true);}
+		mainWindow.setVisible(true);}
 		
 static void naprawKolumny(){
 	while(kolumna>kolMax-2){
 		kolumna-=(kolMax-kolLeft-2);
 		kolOff+=(kolMax-kolLeft-2);
 		if(kolOff==kolMax-kolLeft-2) kolOff++;
-		kolOff2=kolOff-1;	}
+		kolOff2=kolOff-1; }
 		
 	while(kolumna<=kolLeft && kolOff>0){
 		kolumna+=(kolMax-kolLeft-2);
@@ -236,7 +272,6 @@ static void wLewo(JLabel[] labels){
             kolumna=wpisane.get(linia+linOff).length();}}
     else kolumna-=1;}
 
-
 static void wGore(JLabel[] labels){
     if (linia+linOff>0) {
         if(kolOff>0) labels[linia].setText(napiszZero(wpisane.get(linia+linOff)));
@@ -245,10 +280,10 @@ static void wGore(JLabel[] labels){
             linia=linSkok-1;
             linOff-=linSkok;
             refreshRows(labels);}
-    if (kolumna+kolOff2>wpisane.get(linia+linOff).length()) {
-                kolumna=wpisane.get(linia+linOff).length();
-                kolOff=0;
-                kolOff2=0;}}}
+    	if (kolumna+kolOff2>wpisane.get(linia+linOff).length()) {
+			kolumna=wpisane.get(linia+linOff).length();
+			kolOff=0;
+			kolOff2=0;}}}
 
 static void wDol(JLabel[] labels){ 
     if (linia+linOff<wpisane.size()-1){
@@ -354,51 +389,8 @@ static void enteredEnter(JLabel[] labels){
 
 static void refreshRows(JLabel[] labels){
 	for(int i=0;i<linMax;i++){
-         if (i+linOff<wpisane.size()){
-					 if(i==linia) labels[i].setText(napiszAkt(wpisane.get(i+linOff)));
-					 else labels[i].setText(napiszZero(wpisane.get(i+linOff)));}
-         else labels[i].setText(" ");}
-	}}
-
-  @SuppressWarnings("serial")
-  class Kursor extends JPanel{
-  	private int linia=0;
-      private int kolumna=0;
-      private int kurSzer=0;
-	  private int kurWys=0;
-	  private Color textColor;
-	  private Color kurColor;
-  	
-  	Kursor(int linia, int kolumna, int kurSzer, int kurWys, Color textColor, Color kurColor){
-  		this.linia=linia;
-        this.kolumna=kolumna;
-        this.kurSzer=kurSzer;
-		this.kurWys=kurWys;
-		this.textColor = textColor;
-		this.kurColor = kurColor;}
-  		
-  	public void setKursor(int l, int k){
-  		this.linia=l;
-  		this.kolumna=k;}
-    
-
-/*Wersja dla kursywy
-          @Override      
-          public void paintComponent(Graphics g){
-                super.paintComponent(g);
-                Graphics2D g2d = (Graphics2D) g;
-              g2d.setColor(Color.green);
-              g2d.fillRect(3+kurSzer*kolumna,kurWys*linia,kurSzer,kurWys);
-                g2d.setColor(Color.black);
-                g2d.drawRect(3+kurSzer*kolumna,kurWys*linia,kurSzer,kurWys);}*/      
-          
-
-    @Override      
-  	public void paintComponent(Graphics g){
-			super.paintComponent(g);
-			Graphics2D g2d = (Graphics2D) g;
-  		g2d.setColor(kurColor);
-  		g2d.fillRect(kurSzer*kolumna,kurWys*linia,kurSzer,kurWys);
-			g2d.setColor(textColor);
-			g2d.drawRect(kurSzer*kolumna,kurWys*linia,kurSzer,kurWys);
-  }}
+		if (i+linOff<wpisane.size()){
+			if(i==linia) labels[i].setText(napiszAkt(wpisane.get(i+linOff)));
+			else labels[i].setText(napiszZero(wpisane.get(i+linOff)));}
+		else labels[i].setText(" ");}}}  
+				
